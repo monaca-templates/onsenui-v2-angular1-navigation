43e46c13fe0d8b7a61a661efdc37b56f61c9342a
knaito <knaito@asial.co.jp>
2021-05-17 22:37:33 +0900

    feat: Update loader.js to v1.0.4

--- www/components/loader.js
+++ www/components/loader.js
@@ -5,64 +5,6 @@
 
 /*** <GENERATED> ***/
 
-
-/*** <Start:monaca-cordova-loader> ***/
-/*** <Start:monaca-cordova-loader LoadJs:"components/monaca-cordova-loader/cordova-loader.js"> ***/
-(function(){
-  function getDeviceObjectForPreview() {
-    var raw_values = window.location.search.substring(1).split('&');
-    var values = {};
-    var device = { platform: "" };
-    
-    if (raw_values) {
-      for (var key in raw_values) {
-        var tmp = raw_values[key].split('=');
-        values[tmp[0]] = decodeURIComponent(tmp[1]);
-      }
-      device.platform = values.platform;
-    }
-    
-    return device;
-  }
-    
-  if (/^https:\/\/preview-.+monaca\.(local||mobi)/.test(location.href)) {
-    window.device = getDeviceObjectForPreview();
-  }
- 
-  if ((navigator.userAgent.match(/Android/i)) || (navigator.userAgent.match(/iPhone|iPad|iPod/i))) {
-    if (typeof location.href === "string") {
-      var relativePath = location.href.split("/www")[1];
-      var paths = relativePath.split("/");
-      var cordovaJsUrl = ""; 
-      for (var i = 0; i < paths.length - 2; i++) {
-        cordovaJsUrl += "../";
-      }
-      document.write("<script src=\"" + cordovaJsUrl+ "cordova.js" + "\"></script>");
-    }
-  } else if ( ((navigator.userAgent.match(/MSIE\s10.0/)) && (navigator.userAgent.match(/Windows\sNT\s6.2/)) ) || navigator.userAgent.match(/MSAppHost/) || navigator.userAgent.match(/Electron/gi)) {
-    var elm = document.createElement('script');
-    elm.setAttribute("src", "cordova.js");
-    document.getElementsByTagName("head")[0].appendChild(elm);
-  };
-})();
-/*** <End:monaca-cordova-loader LoadJs:"components/monaca-cordova-loader/cordova-loader.js"> ***/
-/*** <End:monaca-cordova-loader> ***/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*** <Start:monaca-core-utils> ***/
 /*** <Start:monaca-core-utils LoadJs:"components/monaca-core-utils/monaca-core-utils.js"> ***/
 /**
@@ -1484,3 +1426,56 @@ window.monaca.cloud = window.monaca.cloud || {};
 ;
 /*** <End:monaca-core-utils LoadJs:"components/monaca-core-utils/monaca-core-utils.js"> ***/
 /*** <End:monaca-core-utils> ***/
+
+/*** <Start:monaca-cordova-loader> ***/
+/*** <Start:monaca-cordova-loader LoadJs:"components/monaca-cordova-loader/cordova-loader.js"> ***/
+(function(){
+  function getDeviceObjectForPreview() {
+    var raw_values = window.location.search.substring(1).split('&');
+    var values = {};
+    var device = { platform: "" };
+
+    if (raw_values) {
+      for (var key in raw_values) {
+        var tmp = raw_values[key].split('=');
+        values[tmp[0]] = decodeURIComponent(tmp[1]);
+      }
+      device.platform = values.platform;
+    }
+
+    return device;
+  }
+
+  if (location && typeof location.href === "string" && /^https:\/\/preview-.+monaca\.(local||mobi)/.test(location.href)) {
+    window.device = getDeviceObjectForPreview();
+  }
+
+  if (
+      (navigator.userAgent.match(/Android/i))
+      || (navigator.userAgent.match(/iPhone|iPad|iPod/i))
+      || (navigator.userAgent.match(/Macintosh; Intel Mac OS X/i) && location.protocol.match(/^https?:/) === null)
+  ) {
+
+    if (typeof location.href === "string") {
+      var cordovaJsUrl = location.protocol + "//" + location.hostname + "/";
+      var relativePath = "";
+      if (location.href.indexOf('/www') !== -1) {
+        relativePath = location.href.split("/www")[1];
+        var paths = relativePath.split("/");
+        cordovaJsUrl = "";
+        for (var i = 0; i < paths.length - 2; i++) {
+          cordovaJsUrl += "../";
+        }
+      }
+      document.write("<script src=\"" + cordovaJsUrl+ "cordova.js" + "\"></script>");
+    }
+  } else if ( (navigator.userAgent.match(/MSIE\s10.0/) && navigator.userAgent.match(/Windows\sNT\s6.2/)) || navigator.userAgent.match(/MSAppHost/)) {
+    var elm = document.createElement('script');
+    elm.setAttribute("src", "cordova.js");
+    document.getElementsByTagName("head")[0].appendChild(elm);
+  };
+})();
+
+;
+/*** <End:monaca-cordova-loader LoadJs:"components/monaca-cordova-loader/cordova-loader.js"> ***/
+/*** <End:monaca-cordova-loader> ***/
